85ae8dc9798cf9edad29e9b4d75a6346745a5d63
Herman Seip <jensse@invenio.as>
2012-03-14 07:07:00 +0100

    Added resource files test and prod

--- src/test/java/as/invenio/jenkins/testHelloJenkins.java
+++ src/test/java/as/invenio/jenkins/testHelloJenkins.java
@@ -1,5 +1,10 @@
 package as.invenio.jenkins;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
 import junit.framework.Assert;
 
 import org.junit.Test;
@@ -7,10 +12,26 @@ import org.junit.Test;
 import as.invenio.jenkins.HelloJenkins;
 
 
-public class testHelloJenkins {
+public class TestHelloJenkins {
 	
 	@Test
 	public void testHelloJenkins(){
-		Assert.assertEquals("HelloJenkins", HelloJenkins.say());
+		Assert.assertEquals("Hello Jenkins", HelloJenkins.say());
+	}
+	@Test
+	public void testTestProperties(){
+		System.out.println(this.getClass().getResourceAsStream("config.properties"));
+	//	System.out.println(ResourceBundle.getBundle("config.properties"));
+		Properties prop = new Properties();
+		try {
+			prop.load(this.getClass().getClassLoader().getResourceAsStream("config.properties"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(prop.getProperty("test"));
+		System.out.println(this.getClass()
+				.getResourceAsStream
+				("config.properties"));
 	}
 }
